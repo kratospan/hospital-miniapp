7818e9cf29a8f6802c8a7d294f50931f1164aa36
kratospan <kratospan@qq.com>
2020-03-20 20:14:40 +0800

    v1.1.3
    1、更改 从云端获取排班日期

--- pages/ask/chooseDate/chooseDate.js
+++ pages/ask/chooseDate/chooseDate.js
@@ -18,7 +18,7 @@ Page({
   },
   
   onReady(){
-	  this.selectSchedulList()
+	  
 	  this.selectDoctor()
   },
   
@@ -36,7 +36,6 @@ Page({
 			  doctor_id : this.data.doctor_id,
 			  schedul_time : e.currentTarget.dataset.index,
 			  has_choose : this.data.has_choose,
-			  
 		  }
 		  
 		  data = JSON.stringify(data)
@@ -48,13 +47,13 @@ Page({
   },
   
   choose_date(e){
-  	  // console.log(e.currentTarget.dataset.target)
   	  this.setData({
   		  has_choose : e.currentTarget.dataset.target
   	  })
   	  this.selectSchedulList()
   },
   
+  //获取日程安排的方法
   selectSchedulList(){
   	  wx.showLoading()
   	  var that = this
@@ -77,45 +76,64 @@ Page({
   	  })
   },
   
+  //用户选择了上午时触发的方法
   chooseAm(){
 	  this.setData({
 		  time : 'am'
 	  })
   },
   
+  //用户选择了下午时触发的方法
   choosePm(){
   	  this.setData({
   		  time : 'pm'
   	  })
   },
   
-  //在本地获取排班日期
+  //在本地获取未来七天的时间
   getTime(){
-  	  var now = new Date()
-  	  var year = now.getFullYear();     //得到年份
-  	  var month = now.getMonth() + 1;       //得到月份
-  	  var date = now.getDate() + 1;         //得到日期
-  	  now = year + '-' + month + '-' + date
-  	  now = Math.round(new Date(now).getTime()/1000)
-  	  var dateList = []
-  	  for(let i = 0;i < 7;i++){
-  		  var nowTime = (now + 60*60*24*i)*1000
-  		  var day = ''
-  		  var array = ['日','一','二','三','四','五','六']
-  		  day = array[new Date(nowTime).getDay()]
-  		  dateList[i] = {
-  			  day : day,
-  			  date : new Date(nowTime).getDate(),
-  			  dateTime : nowTime/1000
-  		  }
-  	  }
-  	  this.setData({
-  		  dateList : dateList,
-  		  has_choose : dateList[0]
-  	  })
+  	  // var now = new Date()
+  	  // var year = now.getFullYear();     	//得到年份
+  	  // var month = now.getMonth() + 1;       //得到月份
+  	  // var date = now.getDate() + 1;         //得到日期
+  	  // now = year + '-' + month + '-' + date
+  	  // now = Math.round(new Date(now).getTime()/1000)
+  	  // var dateList = []
+  	  // for(let i = 0;i < 7;i++){
+  		 //  var nowTime = (now + 60*60*24*i)*1000
+  		 //  var day = ''
+  		 //  var array = ['日','一','二','三','四','五','六']
+  		 //  day = array[new Date(nowTime).getDay()]
+  		 //  dateList[i] = {
+  			//   day : day,
+  			//   date : new Date(nowTime).getDate(),
+  			//   dateTime : nowTime/1000
+  		 //  }
+  	  // }
+	  var that = this
+	  wx.showLoading()
+	  app.gRequest({
+		  url : 'schedul/get_schedul_date',
+		  data : {
+			  token : app.getToken()
+		  }
+	  }).then(function(res){
+		  wx.hideLoading()
+		  if(res.code == 200){
+			  console.log(1)
+			  that.setData({
+			    	dateList : res.data,
+			    	has_choose : res.data[0]
+			  })
+			  that.selectSchedulList()
+		  }else{
+			  app.showModal('获取排班日期错误')
+		  }
+	  })
+  	  
   },
   
-  //获取医生详细信息
+  //获取医生详细信息的方法
   selectDoctor(){
 	  wx.showLoading()
 	  var that = this
